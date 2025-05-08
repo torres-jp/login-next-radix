8cc0180ef5688f9d3412e069a1b7e366b8a6a39b
torres-jp <torres.jpw@gmail.com>
2025-05-08 17:36:11 -0300

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { BookmarkIcon } from '@radix-ui/react-icons'
+import { Button } from '@radix-ui/themes'
+
+function LoginPage() {
+  return (
+    <div>
+      LoginPage
+      <Button>
+        <BookmarkIcon width='16' height='16' /> BookMark
+      </Button>
+    </div>
+  )
+}
+
+export default LoginPage
