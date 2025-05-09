0895e62aa62032c62b69315d7fee1d6d5782d019
torres-jp <torres.jpw@gmail.com>
2025-05-08 23:30:12 -0300

    start wit front

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,16 +1,14 @@
-'use client'
-
-import { BookmarkIcon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
+import { Card, Container, Heading } from '@radix-ui/themes'
 
 function LoginPage() {
   return (
-    <div>
-      LoginPage
-      <Button>
-        <BookmarkIcon width='16' height='16' /> BookMark
-      </Button>
-    </div>
+    <>
+      <Container size='1' height='100%' className='p-3 md:p-0'>
+        <Card>
+          <Heading>Sign in</Heading>
+        </Card>
+      </Container>
+    </>
   )
 }
 
